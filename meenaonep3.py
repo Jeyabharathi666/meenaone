ef9fd50299d8c8566a6ab3b153981d333b34c1d9
Jeyabharathi666 <saravananjeyabharathi@gmail.com>
2026-06-07 13:28:48 +0530

    Update meenaonep3.py

--- meenaonep3.py
+++ meenaonep3.py
@@ -3,39 +3,39 @@ from datetime import datetime
 import google_sheets
 import time
 
-URLS = [
-     "https://chartink.com/screener/copy-copy-copy-explosive-move-day-coming-in-1-2-days",
-     "https://chartink.com/screener/copy-multibagar-5",
-     "https://chartink.com/screener/50-sreelakshmi-guruvayoorappan-b-atr-volume-rocket",
-     "https://chartink.com/screener/50agp-bullish2-p5",
-     "https://chartink.com/screener/50aaa13-vp-sheshapathi",
-     "https://chartink.com/screener/50-oneeeeeee",
-     "https://chartink.com/screener/50shesha-magic-buy-love",
-     "https://chartink.com/screener/50atp-above-long",
-     "https://chartink.com/screener/50-daily-min-f-0-trade",
-     "https://chartink.com/screener/50-the-best-btst",
-     "https://chartink.com/screener/50-22-nw-shesha-magic-buy-love",
-     "https://chartink.com/screener/50-bearish-maribozu",
-     "https://chartink.com/screener/copy-w6-f-o-2",
-     "https://chartink.com/screener/copy-1week-sell-twist",
-     "https://chartink.com/screener/copy-weekly-bollinger-sell-3",
-     "https://chartink.com/screener/sell-postesttttttttttttttttt",
-     "https://chartink.com/screener/copy-cci-below-100-62",
-     "https://chartink.com/screener/copy-bearish-rsi-stoc-1215",
-     "https://chartink.com/screener/srf-narayana-futures-positional-bearish",
-     "https://chartink.com/screener/sell-bollinger-band-weekly-15",
-     "https://chartink.com/screener/copy-bolinger-band-bearish-reversal-aps-401",
-     "https://chartink.com/screener/copy-ut-sell-eod-basis-5",
-     "https://chartink.com/screener/copy-sell-f-0",
-     "https://chartink.com/screener/copy-perfect-bearish-3266",
-     "https://chartink.com/screener/copy-copy-copy-explosive-move-day-coming-in-1-2-days"
-]
+URL = ["https://chartink.com/screener/copy-bearish-engulifing-see-after-3-15-pm-for-next-day-trade-5168",	
+      "https://chartink.com/screener/copy-bearish-maribozu-337",
+ 	  "https://chartink.com/screener/copy-yesterday-and-today-ema3-without-open-high-bearish-55",
+ 	  "https://chartink.com/screener/copy-bearish-engulfing-moderate-478",
+	  "https://chartink.com/screener/agp-bearong-2",
+	  "https://chartink.com/screener/shesha-bearish1",
+	  "https://chartink.com/screener/agp-shesha-bearish-2",
+	  "https://chartink.com/screener/one-rupee-sidh-f-0-sell",
+	  "https://chartink.com/screener/copy-f-o-weak-stocks-2",
+	  "https://chartink.com/screener/svp2-closing-3-up-since-3-days",
+	  "https://chartink.com/screener/copy-copy-how-to-find-future-and-option-stocks-buy-entry-future-3",
+	  "https://chartink.com/screener/copy-stocks-in-downtrend-1959",
+	  "https://chartink.com/screener/copy-w6-f-o-2",
+	  "https://chartink.com/screener/copy-1week-sell-twist",
+	  "https://chartink.com/screener/copy-weekly-bollinger-sell-3",
+	  "https://chartink.com/screener/sell-postesttttttttttttttttt",
+	  "https://chartink.com/screener/copy-cci-below-100-62",
+	  "https://chartink.com/screener/copy-bearish-rsi-stoc-1215".
+	  "https://chartink.com/screener/srf-narayana-futures-positional-bearish",
+	  "https://chartink.com/screener/sell-bollinger-band-weekly-15
+	  "https://chartink.com/screener/copy-bolinger-band-bearish-reversal-aps-401
+	  "https://chartink.com/screener/copy-ut-sell-eod-basis-5",
+	  "https://chartink.com/screener/copy-sell-f-0",
+	  "https://chartink.com/screener/copy-perfect-bearish-3266",
+	  "https://chartink.com/screener/50-bearish-engulifing-see-after-3-15-pm-for-next-day-trade",
+	  "https://chartink.com/screener/50aaaagp-shesha-bearish-2",
+	  "https://chartink.com/screener/50stocks-in-downtrend",
+	  "https://chartink.com/screener/copy-perfect-bearish-3537",
+	  "https://chartink.com/screener/copy-chanakya-bearish-scanner-working-2803"
+	]
        
 sheet_id = "1rTXRUjHkEa9lQMzxelvc_ApFuFz45zKNLeR1SBJ5gTY"
-worksheet_names = [
-    "p1","p2","p3","p4","p5","p6","p7","p8","p9","p10",
-    "p11","p12","p13","p14","p15","p16","p17","p18","p19","p20","p21","p22","p23","p24","p25"]
-
+worksheet_name = ["p1","p2","p3","p4","p5","p6","p7","p8","p9","p10","p11","p12","p13","p14","p15","p16","p17","p18","p19","p20","p21","p22","p23","p24","p25","p26","p27","p28","p29"]
 def scrape_chartink(url, worksheet_name):
     print(f"\n🚀 Starting scrape for '{worksheet_name}'")
     print(f"🌐 Loading URL: {url}")
